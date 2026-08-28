7178efcee0460a47d64f1cb95505f1ee503343c3
meloni13 <melonip137@gmail.com>
2026-08-28 00:28:50 -0700

    Bump version to v2.4.1 (Code 8) for stabilization release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mileowl.tracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.4.0"
+        versionCode = 8
+        versionName = "2.4.1"
     }
 
     buildTypes {
